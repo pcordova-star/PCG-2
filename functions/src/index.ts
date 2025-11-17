4c15b4353b682704259009e675e4a705aebbb39f
Paulo Córdova <pcordova@woken.cl>
2025-11-17 03:35:04 +0000

    firebase deploy Failed to fetch Firebase Project from current Studio Wor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
 import { initializeApp, getApps } from "firebase-admin/app";
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import next from "next";
 
 if (!getApps().length) {
   initializeApp();
 }
 
-// Las funciones callable ahora se manejan a través del servidor de Next.js o como funciones individuales
-// export { createCompanyUser } from './createCompanyUser';
-// export { setSuperAdminClaim } from './setSuperAdmin';
+const isDev = process.env.NODE_ENV !== "production";
+
+const server = next({
+  dev: isDev,
+  conf: { distDir: ".next" },
+});
+
+const nextjsHandle = server.getRequestHandler();
+
+export const nextFramework = onRequest({
+    maxInstances: 2,
+    timeoutSeconds: 300
+}, (req, res) => {
+  return server.prepare().then(() => {
+    logger.info("Request received", { path: req.path });
+    return nextjsHandle(req, res);
+  });
+});
