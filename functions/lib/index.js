dd4c2bf7b92442dbf78455db67064212438cc893
Paulo Córdova <pcordova@woken.cl>
2025-11-17 15:15:53 +0000

    mira la captura, faltan funciones?

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,7 +4,14 @@ const app_1 = require("firebase-admin/app");
 if (!(0, app_1.getApps)().length) {
     (0, app_1.initializeApp)();
 }
-// Las funciones callable ahora se manejan a través del servidor de Next.js o como funciones individuales
-// export { createCompanyUser } from './createCompanyUser';
-// export { setSuperAdminClaim } from './setSuperAdmin';
+// Exportar la función principal de Next.js
+var next_server_1 = require("./next-server");
+Object.defineProperty(exports, "nextFramework", { enumerable: true, get: function () { return next_server_1.nextFramework; } });
+// Exportar las funciones Callable
+var createCompanyUser_1 = require("./createCompanyUser");
+Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
+var setSuperAdmin_1 = require("./setSuperAdmin");
+Object.defineProperty(exports, "setSuperAdminClaim", { enumerable: true, get: function () { return setSuperAdmin_1.setSuperAdminClaim; } });
+var registrarAvanceRapido_1 = require("./registrarAvanceRapido");
+Object.defineProperty(exports, "registrarAvanceRapido", { enumerable: true, get: function () { return registrarAvanceRapido_1.registrarAvanceRapido; } });
 //# sourceMappingURL=index.js.map
