2d5980854a2d1e1914015c00b17ed3b7b7f142ce
Paulo Córdova <pcordova@woken.cl>
2025-12-01 17:17:03 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: useMemo

--- src/app/prevencion/formularios-generales/page.tsx
+++ src/app/prevencion/formularios-generales/page.tsx
@@ -1,7 +1,7 @@
 // src/app/prevencion/formularios-generales/page.tsx
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import {
   collection,
   query,
