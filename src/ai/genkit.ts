7bac5f412c7f0ddfac251d5f50fefa674572b02f
Paulo Córdova <pcordova@woken.cl>
2025-12-10 12:35:34 +0000

    algo pasó

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,6 @@ import { googleAI, gemini25Flash } from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [googleAI()],
   model: gemini25Flash,
-  promptDir: 'prompts/',
 });
 
 export const geminiFlashModel = gemini25Flash;
