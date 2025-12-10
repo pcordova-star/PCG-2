b337e95b78f3e0f24cf873f1a7a7d47d3d702aac
Paulo Córdova <pcordova@woken.cl>
2025-12-10 15:26:24 +0000

    use la url dse vercel para usar el cubicador https://pcg-2.vercel.app/cu

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,9 @@
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/google-genai';
+import 'dotenv/config';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({ apiKey: process.env.GEMINI_API_KEY })],
   model: 'googleai/gemini-2.5-flash',
   promptDir: './prompts/',
 });
