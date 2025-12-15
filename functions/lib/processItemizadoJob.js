0fa6110c07e54098f16dfb945e1abf9daa0291ef
Paulo Córdova <pcordova@woken.cl>
2025-12-15 16:17:09 +0000

    Aquí está el bloqueo real y final, explicado corto y con solución direct

--- functions/lib/processItemizadoJob.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.processItemizadoJob = processItemizadoJob;
-// TEMP: Disabled for deploy. Genkit types cause build issues in this env.
-// Keep export to avoid breaking imports.
-async function processItemizadoJob() {
-    throw new Error("processItemizadoJob deshabilitado temporalmente para deploy.");
-}
-//# sourceMappingURL=processItemizadoJob.js.map
