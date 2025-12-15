7e57a49cd8ac102ef12242b958622c2653624edb
Paulo Córdova <pcordova@woken.cl>
2025-12-15 17:18:12 +0000

    Ya salimos del OOM. Ahora son errores reales de TypeScript en processIte

---
+++ functions/lib/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// redeploy: 2025-12-15
+// src/functions/src/index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSecureDownloadUrl = exports.processItemizadoJob = exports.notifyDocumentDistribution = exports.convertHeicToJpg = exports.registrarAvanceRapido = exports.setSuperAdminClaim = exports.createCompanyUser = void 0;
+/**
+ * Este archivo es el punto de entrada para todas las Cloud Functions.
+ * Cada función se importa desde su propio archivo y se exporta para que Firebase la despliegue.
+ */
+const app_1 = require("firebase-admin/app");
+// import { next } from '@genkit-ai/next';
+// Inicializa Firebase Admin SDK solo si no se ha hecho antes.
+if ((0, app_1.getApps)().length === 0) {
+    (0, app_1.initializeApp)();
+}
+// export { next };
+// Exporta las funciones callable para que estén disponibles en el backend.
+// El nombre de la propiedad del objeto exportado será el nombre de la función en Firebase.
+var createCompanyUser_1 = require("./createCompanyUser");
+Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
+var setSuperAdmin_1 = require("./setSuperAdmin");
+Object.defineProperty(exports, "setSuperAdminClaim", { enumerable: true, get: function () { return setSuperAdmin_1.setSuperAdminClaim; } });
+var registrarAvanceRapido_1 = require("./registrarAvanceRapido");
+Object.defineProperty(exports, "registrarAvanceRapido", { enumerable: true, get: function () { return registrarAvanceRapido_1.registrarAvanceRapido; } });
+var convertHeic_1 = require("./convertHeic");
+Object.defineProperty(exports, "convertHeicToJpg", { enumerable: true, get: function () { return convertHeic_1.convertHeicToJpg; } });
+var notifyDocumentDistribution_1 = require("./notifyDocumentDistribution");
+Object.defineProperty(exports, "notifyDocumentDistribution", { enumerable: true, get: function () { return notifyDocumentDistribution_1.notifyDocumentDistribution; } });
+var processItemizadoJob_1 = require("./processItemizadoJob");
+Object.defineProperty(exports, "processItemizadoJob", { enumerable: true, get: function () { return processItemizadoJob_1.processItemizadoJob; } });
+var secureDownload_1 = require("./secureDownload");
+Object.defineProperty(exports, "getSecureDownloadUrl", { enumerable: true, get: function () { return secureDownload_1.getSecureDownloadUrl; } });
+//# sourceMappingURL=index.js.map
