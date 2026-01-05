97500412eb6716e5c64f66b0f68c38d17d06281d
Paulo Córdova <pcordova@woken.cl>
2026-01-05 22:38:19 +0000

    Verifica index.ts
    Debe verse algo así, sin v2:
    
    export { createCompanyU

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -1,9 +1,4 @@
 // src/functions/src/index.ts
-
-/**
- * Este archivo es el punto de entrada para todas las Cloud Functions.
- * Cada función se importa desde su propio archivo y se exporta para que Firebase la despliegue.
- */
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
@@ -15,23 +10,12 @@ if (!admin.apps.length) {
 // Establece la región global para todas las funciones v1
 const region = "southamerica-west1";
 
-// Exporta las funciones v1 y v2
-const v1Exports = {
-  createCompanyUser: functions.region(region).https.onCall(async (data, context) => {
-    const { createCompanyUser: createCompanyUserV2 } = await import('./createCompanyUser');
-    return createCompanyUserV2(data, context);
-  }),
-  setSuperAdminClaim: functions.region(region).https.onCall(async (data, context) => {
-    const { setSuperAdminClaim: setSuperAdminClaimV2 } = await import('./setSuperAdmin');
-    return setSuperAdminClaimV2(data, context);
-  }),
-};
+// Exporta las funciones v1 (callable)
+export { createCompanyUser } from "./createCompanyUser";
+export { setSuperAdminClaim } from "./setSuperAdmin";
+export { registrarAvanceRapido } from "./registrarAvanceRapido";
+export { notifyDocumentDistribution } from "./notifyDocumentDistribution";
 
-const { registrarAvanceRapido } = require('./registrarAvanceRapido');
-const v2Exports = require('./index.v2');
-
-module.exports = {
-  ...v1Exports,
-  ...v2Exports,
-  registrarAvanceRapido,
-};
+// Exporta las funciones v2 (storage y firestore triggers) por separado
+export { convertHeicToJpg } from './convertHeic';
+export { processItemizadoJob } from './processItemizadoJob';
