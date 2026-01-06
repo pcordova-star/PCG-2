fdf01af92390a1f073b51ff5412d595e559a3234
Paulo Córdova <pcordova@woken.cl>
2026-01-06 19:07:47 +0000

    Firma incorrecta de onDocumentCreated
    Estás usando algo así:
    
    onDocumen

--- functions/gen2/lib/index.js
+++ functions/gen2/lib/index.js
@@ -4,13 +4,12 @@
  * Este archivo es el punto de entrada para todas las Cloud Functions de Gen2.
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.processItemizadoJob = exports.convertHeicToJpg = exports.deactivateCompanyUser = void 0;
+exports.convertHeicToJpg = exports.deactivateCompanyUser = void 0;
 // --- Funciones HTTP (Gen2 onRequest) ---
 var deactivateCompanyUser_1 = require("./deactivateCompanyUser");
 Object.defineProperty(exports, "deactivateCompanyUser", { enumerable: true, get: function () { return deactivateCompanyUser_1.deactivateCompanyUser; } });
 // --- Triggers de eventos (Gen2) ---
 var convertHeic_1 = require("./convertHeic");
 Object.defineProperty(exports, "convertHeicToJpg", { enumerable: true, get: function () { return convertHeic_1.convertHeicToJpg; } });
-var processItemizadoJob_1 = require("./processItemizadoJob");
-Object.defineProperty(exports, "processItemizadoJob", { enumerable: true, get: function () { return processItemizadoJob_1.processItemizadoJob; } });
+// export { processItemizadoJob } from './processItemizadoJob';
 //# sourceMappingURL=index.js.map
