ede908969568cb608cbb263baa610d9c6b7b73cc
Paulo Córdova <pcordova@woken.cl>
2026-01-06 13:43:09 +0000

    Actúa como ingeniero senior experto en Firebase Cloud Functions.
    Contex

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,8 @@ export { createCompanyUser } from "./createCompanyUser";
 export { setSuperAdminClaim } from "./setSuperAdmin";
 export { registrarAvanceRapido } from "./registrarAvanceRapido";
 export { notifyDocumentDistribution } from "./notifyDocumentDistribution";
+export { checkUserExistsByEmail } from "./checkUserExistsByEmail";
+export { testGoogleAi } from "./test-google-ai";
 
 // Exporta las funciones v2 (storage y firestore triggers) por separado
 export { convertHeicToJpg } from './convertHeic';
