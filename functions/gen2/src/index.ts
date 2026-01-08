0e122651561cd5ec57a93933e8b1626692368526
Paulo Córdova <pcordova@woken.cl>
2026-01-08 14:45:15 +0000

    Editar el index real GEN2
    Abre:
    
    nano /home/user/studio/functions/gen2/

--- functions/gen2/src/index.ts
+++ functions/gen2/src/index.ts
@@ -1,12 +1,9 @@
 // functions/gen2/src/index.ts
-/**
- * Este archivo es el punto de entrada para todas las Cloud Functions de Gen2.
- */
 
 // --- Funciones HTTP (Gen2 onRequest) ---
 export { deactivateCompanyUser } from "./deactivateCompanyUser";
 export { requestModuleActivation } from "./requestModuleActivation";
 
 // --- Triggers de eventos (Gen2) ---
-export { convertHeicToJpg } from './convertHeic';
-// export { processItemizadoJob } from './processItemizadoJob';
+export { convertHeicToJpg } from "./convertHeic";
+// export { processItemizadoJob } from "./processItemizadoJob";
