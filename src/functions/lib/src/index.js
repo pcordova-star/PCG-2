af1718d12b44c6e4379584e0d25e023d31788c9d
Paulo Córdova <pcordova@woken.cl>
2026-01-08 18:57:46 +0000

    Copia y reemplaza COMPLETO:
    /home/user/studio/src/functions/src/index.t

---
+++ src/functions/lib/src/index.js
@@ -0,0 +1,42 @@
+"use strict";
+// src/functions/src/index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.processItemizadoJob = exports.convertHeicToJpg = exports.testGoogleAi = exports.requestModuleActivation = exports.setSuperAdminClaim = exports.deactivateCompanyUser = exports.notifyDocumentDistribution = exports.registrarAvanceRapido = exports.createCompanyUser = void 0;
+/**
+ * Este archivo es el punto de entrada para todas las Cloud Functions.
+ * Cada función se importa desde su propio archivo y se exporta para que Firebase la despliegue.
+ */
+const app_1 = require("firebase-admin/app");
+const v2_1 = require("firebase-functions/v2");
+// Inicializa Firebase Admin SDK solo si no se ha hecho antes.
+if ((0, app_1.getApps)().length === 0) {
+    (0, app_1.initializeApp)();
+}
+// Establece opciones globales para todas las funciones v2, asegurando la región
+// y la cuenta de servicio con permisos para acceder a secretos.
+(0, v2_1.setGlobalOptions)({
+    region: "southamerica-west1",
+    serviceAccount: "pcg-functions-sa@pcg-2-8bf1b.iam.gserviceaccount.com"
+});
+// --- Exportación de funciones ---
+// Funciones v1 (callable y http)
+var createCompanyUser_1 = require("./createCompanyUser");
+Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
+var registrarAvanceRapido_1 = require("./registrarAvanceRapido");
+Object.defineProperty(exports, "registrarAvanceRapido", { enumerable: true, get: function () { return registrarAvanceRapido_1.registrarAvanceRapido; } });
+var notifyDocumentDistribution_1 = require("./notifyDocumentDistribution");
+Object.defineProperty(exports, "notifyDocumentDistribution", { enumerable: true, get: function () { return notifyDocumentDistribution_1.notifyDocumentDistribution; } });
+var deactivateCompanyUser_1 = require("./deactivateCompanyUser");
+Object.defineProperty(exports, "deactivateCompanyUser", { enumerable: true, get: function () { return deactivateCompanyUser_1.deactivateCompanyUser; } });
+var setSuperAdmin_1 = require("./setSuperAdmin");
+Object.defineProperty(exports, "setSuperAdminClaim", { enumerable: true, get: function () { return setSuperAdmin_1.setSuperAdminClaim; } });
+var requestModuleActivation_1 = require("./requestModuleActivation");
+Object.defineProperty(exports, "requestModuleActivation", { enumerable: true, get: function () { return requestModuleActivation_1.requestModuleActivation; } });
+// export { checkUserExistsByEmail } from "./checkUserExistsByEmail";
+var test_google_ai_1 = require("./test-google-ai");
+Object.defineProperty(exports, "testGoogleAi", { enumerable: true, get: function () { return test_google_ai_1.testGoogleAi; } });
+// Funciones v2 (triggers de evento)
+var convertHeic_1 = require("./convertHeic");
+Object.defineProperty(exports, "convertHeicToJpg", { enumerable: true, get: function () { return convertHeic_1.convertHeicToJpg; } });
+var processItemizadoJob_1 = require("./processItemizadoJob");
+Object.defineProperty(exports, "processItemizadoJob", { enumerable: true, get: function () { return processItemizadoJob_1.processItemizadoJob; } });
