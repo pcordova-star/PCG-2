2400e3af8b604362feaba41541ae7d3b0981773b
Paulo Córdova <pcordova@woken.cl>
2026-01-08 19:14:48 +0000

    Arreglar el import del scheduler, con la ruta correcta
    Tu entrypoint:

--- src/functions/lib/index.js
+++ src/functions/lib/index.js
@@ -1,13 +1,13 @@
 "use strict";
 /**
  * Punto de entrada REAL de Cloud Functions.
- * Solo exportamos funciones que existen en src/
+ * Exportamos solo funciones que existen en la carpeta src/
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.requestModuleActivation = exports.mclpDailyScheduler = exports.createCompanyUser = void 0;
-var createCompanyUser_1 = require("./createCompanyUser");
+var createCompanyUser_1 = require("./src/createCompanyUser");
 Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
-var scheduler_1 = require("./mclp/scheduler");
+var scheduler_1 = require("./src/mclp/scheduler");
 Object.defineProperty(exports, "mclpDailyScheduler", { enumerable: true, get: function () { return scheduler_1.mclpDailyScheduler; } });
-var requestModuleActivation_1 = require("./requestModuleActivation");
+var requestModuleActivation_1 = require("./src/requestModuleActivation");
 Object.defineProperty(exports, "requestModuleActivation", { enumerable: true, get: function () { return requestModuleActivation_1.requestModuleActivation; } });
