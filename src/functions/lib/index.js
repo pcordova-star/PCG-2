d410451ffa8cc544782bd46ef78d4ee7a26e476f
Paulo Córdova <pcordova@woken.cl>
2026-01-09 04:05:43 +0000

    > build > tsc
    src/convertHeic.ts:8:25 - error TS7016: Could not find a

--- src/functions/lib/index.js
+++ src/functions/lib/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// src/functions/index.ts
+// src/functions/src/index.ts
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mclpDailyScheduler = exports.processItemizadoJob = exports.convertHeicToJpg = exports.checkUserExistsByEmail = exports.registrarAvanceRapido = exports.testGoogleAi = exports.setSuperAdminClaim = exports.requestModuleActivation = exports.notifyDocumentDistribution = exports.deactivateCompanyUser = exports.createCompanyUser = void 0;
 /**
@@ -20,27 +20,28 @@ if ((0, app_1.getApps)().length === 0) {
 });
 // --- Exportación de funciones ---
 // Funciones v2 (callable, http, triggers)
-var createCompanyUser_1 = require("./src/createCompanyUser");
+var createCompanyUser_1 = require("./createCompanyUser");
 Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
-var deactivateCompanyUser_1 = require("./src/deactivateCompanyUser");
+var deactivateCompanyUser_1 = require("./deactivateCompanyUser");
 Object.defineProperty(exports, "deactivateCompanyUser", { enumerable: true, get: function () { return deactivateCompanyUser_1.deactivateCompanyUser; } });
-var notifyDocumentDistribution_1 = require("./src/notifyDocumentDistribution");
+var notifyDocumentDistribution_1 = require("./notifyDocumentDistribution");
 Object.defineProperty(exports, "notifyDocumentDistribution", { enumerable: true, get: function () { return notifyDocumentDistribution_1.notifyDocumentDistribution; } });
-var requestModuleActivation_1 = require("./src/requestModuleActivation");
+var requestModuleActivation_1 = require("./requestModuleActivation");
 Object.defineProperty(exports, "requestModuleActivation", { enumerable: true, get: function () { return requestModuleActivation_1.requestModuleActivation; } });
-var setSuperAdmin_1 = require("./src/setSuperAdmin");
+var setSuperAdmin_1 = require("./setSuperAdmin");
 Object.defineProperty(exports, "setSuperAdminClaim", { enumerable: true, get: function () { return setSuperAdmin_1.setSuperAdminClaim; } });
-var test_google_ai_1 = require("./src/test-google-ai");
+var test_google_ai_1 = require("./test-google-ai");
 Object.defineProperty(exports, "testGoogleAi", { enumerable: true, get: function () { return test_google_ai_1.testGoogleAi; } });
-var registrarAvanceRapido_1 = require("./src/registrarAvanceRapido");
+var registrarAvanceRapido_1 = require("./registrarAvanceRapido");
 Object.defineProperty(exports, "registrarAvanceRapido", { enumerable: true, get: function () { return registrarAvanceRapido_1.registrarAvanceRapido; } });
-var checkUserExistsByEmail_1 = require("./src/checkUserExistsByEmail");
+var checkUserExistsByEmail_1 = require("./checkUserExistsByEmail");
 Object.defineProperty(exports, "checkUserExistsByEmail", { enumerable: true, get: function () { return checkUserExistsByEmail_1.checkUserExistsByEmail; } });
 // Triggers
-var convertHeic_1 = require("./src/convertHeic");
+var convertHeic_1 = require("./convertHeic");
 Object.defineProperty(exports, "convertHeicToJpg", { enumerable: true, get: function () { return convertHeic_1.convertHeicToJpg; } });
-var processItemizadoJob_1 = require("./src/processItemizadoJob");
+var processItemizadoJob_1 = require("./processItemizadoJob");
 Object.defineProperty(exports, "processItemizadoJob", { enumerable: true, get: function () { return processItemizadoJob_1.processItemizadoJob; } });
 // Tareas programadas
-var scheduler_1 = require("./src/mclp/scheduler");
+var scheduler_1 = require("./mclp/scheduler");
 Object.defineProperty(exports, "mclpDailyScheduler", { enumerable: true, get: function () { return scheduler_1.mclpDailyScheduler; } });
+//# sourceMappingURL=index.js.map
---
+++ src/functions/src/types/heic-convert.d.ts
@@ -0,0 +1,2 @@
+// Declara el módulo para que TypeScript no se queje de la falta de tipos.
+declare module "heic-convert";
