7f8233e14217e18ab47ac4ee70f692ea17989fef
Paulo Córdova <pcordova@woken.cl>
2026-01-11 15:05:52 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -5,23 +5,18 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    // Esta función solo se ejecutará en el cliente, después del montaje inicial.
     const checkIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     };
 
-    // Establecer el valor inicial
     checkIsMobile();
-
-    // Escuchar cambios
     window.addEventListener("resize", checkIsMobile);
     
-    // Limpieza al desmontar
     return () => window.removeEventListener("resize", checkIsMobile);
-  }, []) // El array de dependencias vacío asegura que se ejecute solo una vez en el cliente
+  }, [])
 
   return isMobile
 }
