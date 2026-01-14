8ebbec20bac2a6401d8bbf69d9437163fc11153f
Paulo Córdova <pcordova@woken.cl>
2026-01-14 15:13:13 +0000

    1. Busca en tu proyecto cualquier variable con el error:
    En VSCode pres

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -9,7 +9,7 @@ const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "pcg-2-8bf1b.firebasestorage.app", // Hardcoded para evitar errores de derivación
+  storageBucket: "pcg-2-8bf1b.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
