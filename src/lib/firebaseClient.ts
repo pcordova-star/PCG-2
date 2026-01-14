38d3ac2280ccf7e950df30db8133fab703cab007
Paulo Córdova <pcordova@woken.cl>
2026-01-14 15:46:56 +0000

    aplica los cambios

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -10,7 +10,7 @@ const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "pcg-2-8bf1b.firebasestorage.app", // Fallback por seguridad
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
