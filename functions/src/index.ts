38df7e26730b9af037a0977503cd0b89b0f3f271
Paulo Córdova <pcordova@woken.cl>
2026-01-18 22:45:19 +0000

    Edita el archivo nano functions/src/index.ts
    Déjalo EXACTAMENTE así:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
-// functions/src/index.ts
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
+// Funciones activas
 export { createCompanyUser } from "./createCompanyUser";
 export { analizarPlano } from "./analizarPlano";
-export { processItemizadoJob } from "./processItemizadoJob";
