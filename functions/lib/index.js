d98afaf852aa66b04fc58a5f34caa54dfed4e068
Paulo Córdova <pcordova@woken.cl>
2026-01-18 22:09:42 +0000

    Crear un archivo tsconfig exclusivo que SOLO compile index.ts, createCom

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -33,38 +33,12 @@ var __importStar = (this && this.__importStar) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mclpDailyScheduler = exports.processItemizadoJob = exports.convertHeicToJpg = exports.testGoogleAi = exports.setSuperAdminClaim = exports.setCompanyClaims = exports.requestModuleActivation = exports.notifyDocumentDistribution = exports.registrarAvanceRapido = exports.deactivateCompanyUser = exports.checkUserExistsByEmail = exports.analizarPlano = exports.createCompanyUser = void 0;
-// src/functions/src/index.ts
+exports.analizarPlano = exports.createCompanyUser = void 0;
 const admin = __importStar(require("firebase-admin"));
 admin.initializeApp();
-// --- Funciones principales ---
+// Funciones activas
 var createCompanyUser_1 = require("./createCompanyUser");
 Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
 var analizarPlano_1 = require("./analizarPlano");
 Object.defineProperty(exports, "analizarPlano", { enumerable: true, get: function () { return analizarPlano_1.analizarPlano; } });
-// --- Funciones auxiliares ---
-var checkUserExistsByEmail_1 = require("./checkUserExistsByEmail");
-Object.defineProperty(exports, "checkUserExistsByEmail", { enumerable: true, get: function () { return checkUserExistsByEmail_1.checkUserExistsByEmail; } });
-var deactivateCompanyUser_1 = require("./deactivateCompanyUser");
-Object.defineProperty(exports, "deactivateCompanyUser", { enumerable: true, get: function () { return deactivateCompanyUser_1.deactivateCompanyUser; } });
-var registrarAvanceRapido_1 = require("./registrarAvanceRapido");
-Object.defineProperty(exports, "registrarAvanceRapido", { enumerable: true, get: function () { return registrarAvanceRapido_1.registrarAvanceRapido; } });
-var notifyDocumentDistribution_1 = require("./notifyDocumentDistribution");
-Object.defineProperty(exports, "notifyDocumentDistribution", { enumerable: true, get: function () { return notifyDocumentDistribution_1.notifyDocumentDistribution; } });
-var requestModuleActivation_1 = require("./requestModuleActivation");
-Object.defineProperty(exports, "requestModuleActivation", { enumerable: true, get: function () { return requestModuleActivation_1.requestModuleActivation; } });
-var setCompanyClaims_1 = require("./setCompanyClaims");
-Object.defineProperty(exports, "setCompanyClaims", { enumerable: true, get: function () { return setCompanyClaims_1.setCompanyClaims; } });
-var setSuperAdmin_1 = require("./setSuperAdmin");
-Object.defineProperty(exports, "setSuperAdminClaim", { enumerable: true, get: function () { return setSuperAdmin_1.setSuperAdminClaim; } });
-var test_google_ai_1 = require("./test-google-ai");
-Object.defineProperty(exports, "testGoogleAi", { enumerable: true, get: function () { return test_google_ai_1.testGoogleAi; } });
-// --- Triggers ---
-var convertHeic_1 = require("./convertHeic");
-Object.defineProperty(exports, "convertHeicToJpg", { enumerable: true, get: function () { return convertHeic_1.convertHeicToJpg; } });
-var processItemizadoJob_1 = require("./processItemizadoJob");
-Object.defineProperty(exports, "processItemizadoJob", { enumerable: true, get: function () { return processItemizadoJob_1.processItemizadoJob; } });
-// --- Scheduler ---
-var scheduler_1 = require("./mclp/scheduler");
-Object.defineProperty(exports, "mclpDailyScheduler", { enumerable: true, get: function () { return scheduler_1.mclpDailyScheduler; } });
 //# sourceMappingURL=index.js.map
