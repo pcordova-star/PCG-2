444131d84596fd6e45a4ae353a2c54ab7903fee9
Paulo Córdova <pcordova@woken.cl>
2026-01-18 18:23:48 +0000

    todo pasa por un tema de gen, ahora sigue el problema, creo que antes la

--- functions/gen2/src/index.ts
+++ functions/gen2/src/index.ts
@@ -3,7 +3,8 @@
 // --- Funciones HTTP (Gen2 onRequest) ---
 export { deactivateCompanyUser } from "./deactivateCompanyUser";
 export { requestModuleActivation } from "./requestModuleActivation";
+export { analizarPlano } from "./analizarPlano"; // <-- Función añadida aquí
 
 // --- Triggers de eventos (Gen2) ---
 export { convertHeicToJpg } from "./convertHeic";
-// export { processItemizadoJob } from "./processItemizadoJob";
+export { processItemizadoJob } from './processItemizadoJob';
