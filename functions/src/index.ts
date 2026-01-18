ce02d96ba48d750ca258d65e3987e7b39693f2ec
Paulo Córdova <pcordova@woken.cl>
2026-01-18 21:59:06 +0000

    Edita el archivo functions/src/index.ts Y deja el contenido así:
    import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,6 @@
-// src/functions/src/index.ts
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-// --- Funciones principales ---
+// Funciones activas
 export { createCompanyUser } from "./createCompanyUser";
 export { analizarPlano } from "./analizarPlano";
-
-// --- Funciones auxiliares ---
-export { checkUserExistsByEmail } from "./checkUserExistsByEmail";
-export { deactivateCompanyUser } from "./deactivateCompanyUser";
-export { registrarAvanceRapido } from "./registrarAvanceRapido";
-export { notifyDocumentDistribution } from "./notifyDocumentDistribution";
-export { requestModuleActivation } from "./requestModuleActivation";
-export { setCompanyClaims } from "./setCompanyClaims";
-export { setSuperAdminClaim } from "./setSuperAdmin";
-export { testGoogleAi } from "./test-google-ai";
-
-// --- Triggers ---
-export { convertHeicToJpg } from "./convertHeic";
-export { processItemizadoJob } from "./processItemizadoJob";
-
-// --- Scheduler ---
-export { mclpDailyScheduler } from "./mclp/scheduler";
