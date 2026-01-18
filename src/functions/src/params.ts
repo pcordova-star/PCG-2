68f450ac8441199ef5d775e5e6c19005491d4ca8
Paulo Córdova <pcordova@woken.cl>
2026-01-18 00:18:31 +0000

    la api estaba comprometida, la borré, tengo otra hagamos denuevo el depl

--- src/functions/src/params.ts
+++ src/functions/src/params.ts
@@ -5,4 +5,4 @@ import { defineSecret } from 'firebase-functions/params';
  * Define el secreto para la API Key de Gemini con un nombre único para el proyecto.
  * Esto permite que las Cloud Functions de 2ª Generación accedan al secreto de forma segura y evita conflictos.
  */
-export const PPCG_GEMINI_API_KEY_SECRET = defineSecret('PPCG_GEMINI_API_KEY');
+export const PPCG_GEMINI_API_KEY_SECRET = defineSecret('PPCG_GEMINI_API_KEY_V2');
