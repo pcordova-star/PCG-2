f8ae5f4b04ff0e54c2dc7e3340b212a41a19c0c7
Paulo Córdova <pcordova@woken.cl>
2026-01-18 21:41:24 +0000

    NECESITO QUE PEGUES AQUÍ COMPLETO el archivo: functions/src/analizarPlan

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -33,42 +33,13 @@ var __importStar = (this && this.__importStar) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mclpDailyScheduler = exports.testGoogleAi = exports.analizarPlano = exports.processItemizadoJob = exports.convertHeicToJpg = exports.notifyDocumentDistribution = exports.requestModuleActivation = exports.registrarAvanceRapido = exports.deactivateCompanyUser = exports.setCompanyClaims = exports.setSuperAdminClaim = exports.checkUserExistsByEmail = exports.createCompanyUser = void 0;
+exports.analizarPlano = exports.createCompanyUser = void 0;
 // src/functions/src/index.ts
 const admin = __importStar(require("firebase-admin"));
 admin.initializeApp();
-/**
- * Este archivo es el punto de entrada para todas las Cloud Functions.
- * Cada función se importa desde su propio archivo y se exporta para que Firebase la despliegue.
- */
-// --- Exportación de funciones v1 ---
+// Funciones activas
 var createCompanyUser_1 = require("./createCompanyUser");
 Object.defineProperty(exports, "createCompanyUser", { enumerable: true, get: function () { return createCompanyUser_1.createCompanyUser; } });
-var checkUserExistsByEmail_1 = require("./checkUserExistsByEmail");
-Object.defineProperty(exports, "checkUserExistsByEmail", { enumerable: true, get: function () { return checkUserExistsByEmail_1.checkUserExistsByEmail; } });
-var setSuperAdmin_1 = require("./setSuperAdmin");
-Object.defineProperty(exports, "setSuperAdminClaim", { enumerable: true, get: function () { return setSuperAdmin_1.setSuperAdminClaim; } });
-var setCompanyClaims_1 = require("./setCompanyClaims");
-Object.defineProperty(exports, "setCompanyClaims", { enumerable: true, get: function () { return setCompanyClaims_1.setCompanyClaims; } });
-var deactivateCompanyUser_1 = require("./deactivateCompanyUser");
-Object.defineProperty(exports, "deactivateCompanyUser", { enumerable: true, get: function () { return deactivateCompanyUser_1.deactivateCompanyUser; } });
-var registrarAvanceRapido_1 = require("./registrarAvanceRapido");
-Object.defineProperty(exports, "registrarAvanceRapido", { enumerable: true, get: function () { return registrarAvanceRapido_1.registrarAvanceRapido; } });
-var requestModuleActivation_1 = require("./requestModuleActivation");
-Object.defineProperty(exports, "requestModuleActivation", { enumerable: true, get: function () { return requestModuleActivation_1.requestModuleActivation; } });
-var notifyDocumentDistribution_1 = require("./notifyDocumentDistribution");
-Object.defineProperty(exports, "notifyDocumentDistribution", { enumerable: true, get: function () { return notifyDocumentDistribution_1.notifyDocumentDistribution; } });
-// --- Triggers (Storage, Firestore) v1 ---
-var convertHeic_1 = require("./convertHeic");
-Object.defineProperty(exports, "convertHeicToJpg", { enumerable: true, get: function () { return convertHeic_1.convertHeicToJpg; } });
-var processItemizadoJob_1 = require("./processItemizadoJob");
-Object.defineProperty(exports, "processItemizadoJob", { enumerable: true, get: function () { return processItemizadoJob_1.processItemizadoJob; } });
-// --- Funciones de IA ---
 var analizarPlano_1 = require("./analizarPlano");
 Object.defineProperty(exports, "analizarPlano", { enumerable: true, get: function () { return analizarPlano_1.analizarPlano; } });
-var test_google_ai_1 = require("./test-google-ai");
-Object.defineProperty(exports, "testGoogleAi", { enumerable: true, get: function () { return test_google_ai_1.testGoogleAi; } });
-// --- Funciones Programadas (Scheduler) v1 ---
-var scheduler_1 = require("./mclp/scheduler");
-Object.defineProperty(exports, "mclpDailyScheduler", { enumerable: true, get: function () { return scheduler_1.mclpDailyScheduler; } });
 //# sourceMappingURL=index.js.map
